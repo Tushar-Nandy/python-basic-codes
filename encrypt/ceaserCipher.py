47fecf3106963d5ff138e668514d543fb3bb89b9
Scorpion King(Tushar) <tusharnandy0910@gmail.com>
2024-05-22 19:59:51 +0530

    created a single function for decrypt and encrypt made ascii art and improved user experience

--- encrypt/ceaserCipher.py
+++ encrypt/ceaserCipher.py
@@ -1,36 +1,29 @@
+from asciiart import logo
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 n=len(alphabet)
 punctuation=[',','.',' ','!','$','"',"'","(",')','1','2','3','4','5','6','7','8','9','0','-','_','/','@']
-def encrypt(shift,letter):    
-    encodeLetter=''
+def caser(shift,letter,opr):
+    text=''
+    if opr=="decrypt":
+        shift*=-1
     for word in letter:
         if word in punctuation:
-            encodeLetter+=word
+            text+=word
         else:
-            encodeLetter+=alphabet[(alphabet.index(word)+shift)%n]
-    print(f"The encoded message is {encodeLetter}")
-
-def decrypt(shift,letter):
-    decode=''
-    for word in letter:
-        if word in punctuation:
-            decode+=word
-        else:
-            index=alphabet.index(word)-shift
+            index=alphabet.index(word)+shift
             if index>=0:
-                decode+=alphabet[index]
-            else:
-                decode+=alphabet[index+n]
-    print(f"The decoded message is {decode}")
+                index=index%n
+            text+=alphabet[index]
+    print(f"The {opr}ed word is: {text}")
+
 
 def main():
     shift=int(input("Enter the encoding (shifting) number: "))
     letter=input("Enter the message: ").lower()
-    operation=input("What you want to do encrypt(e) or decrypt(d): ").lower()
-    if operation=='e':
-        encrypt(shift,letter)
-    elif operation=='d':
-        decrypt(shift,letter)
-    else:
+    operation=input("What you want to do encrypt or decrypt: ").lower()
+    if operation !='decrypt' and operation !="encrypt":
         print("Invalid operation")
+    else:
+        caser(shift,letter,operation)
 main()
