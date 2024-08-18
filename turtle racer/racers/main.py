f04f3851107b8b430bb7d6c45788a771326aa5b9
Scorpion King(Tushar) <tusharnandy0910@gmail.com>
2024-08-18 22:10:44 +0530

    finally completed the turtle racer game took me a while to figure out the logic and a lot of motivation to keep going enjoyed the process and learned a lot

--- turtle racer/racers/main.py
+++ turtle racer/racers/main.py
@@ -2,22 +2,36 @@ import turtle as t
 import random as rd
 X=-230
 Y=-70
+is_race_on=False
 color=['purple','blue','green','yellow','orange','red']
+all_turtles=[]
 screen=t.Screen()
 screen.setup(width=500,height=400)
 user_bet=screen.textinput(title="Make your bet",prompt="Which turtle (red/blue/green/orange/purple/yellow)")
 
 for turtle_index in range(6):
-    tim=t.Turtle(shape="turtle")
-    tim.penup()
-    tim.color(color[turtle_index])
-    tim.goto(X,Y)
+    new_turtle=t.Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(color[turtle_index])
+    new_turtle.goto(X,Y)
+    all_turtles.append(new_turtle)
     Y+=30
 
 if user_bet:
     is_race_on=True   
 
 while is_race_on:
-    random_distance=rd.randint(0,10)
-print(user_bet)
+    for turle in all_turtles:
+        if turle.xcor()>220:
+            is_race_on=False
+            win_color=turle.pencolor()
+            if win_color==user_bet:
+                 print(f"You won!! The {win_color} turtle is the winner")
+                 break
+            else:
+             print(f"You lose! The {win_color} turtle won")
+             break
+        random_distance=rd.randint(0,10) #takes number betweeen 0 and 10
+        turle.forward(random_distance)
+
 screen.exitonclick() 
