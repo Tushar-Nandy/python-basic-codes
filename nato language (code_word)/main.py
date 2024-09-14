58d32c0b2c622b4e5f376921e3f75b36e2d869e6
Scorpion King(Tushar) <tusharnandy0910@gmail.com>
2024-09-14 16:02:52 +0530

    modified nato language (code_word)/main.py to include error handling for invalid input

--- nato language (code_word)/main.py
+++ nato language (code_word)/main.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
 data_csv=pd.read_csv("nato_phonetic_alphabet.csv")
-
+is_on=True
 data_dict={row.letter : row.code for (index,row) in data_csv.iterrows()}
-print(data_dict)
+#print(data_dict)
 
-name=input("Enter a string: ")
-
-name_list=[data_dict[char.upper()] for char in name ]
-
+while is_on:
+    name=input("Enter a string: ")
+    try:
+        name_list=[data_dict[char.upper()] for char in name ]
+    except:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        is_on=False
 print(name_list)
